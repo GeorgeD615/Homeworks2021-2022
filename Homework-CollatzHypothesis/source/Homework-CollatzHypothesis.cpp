157a92ebe13633c9eea944479afe82653ed5d5c3
GeorgeD615 <st095277@student.spbu.ru>
2021-10-19 22:02:32 +0300

    task

---
+++ Homework-CollatzHypothesis/source/Homework-CollatzHypothesis.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+    int n;
+    do {
+        std::cout << "Input n:";
+        std::cin >> n;
+    } while (n < 1);
+    while (n != 1) {
+        std::cout << n << " ";
+        if (n % 2 == 0) {
+            n /= 2;
+        }
+        else {
+            n *= 3;
+            ++n;
+        }
+    }
+    std::cout << 1;
+}
